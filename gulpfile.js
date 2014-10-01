6a2a1758bb6f221f7dce4d2249e943f7cf8f462c
Will <will@arkade.com.au>
2014-10-01 17:46:36 +1000

    first

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp      = require('gulp'),
+    connect   = require('gulp-connect'),
+    data      = require('gulp-data'),
+    template  = require('gulp-template'),
+    concat    = require('gulp-concat'),
+    path      = require('path'),
+    fs        = require('fs'),
+    sass      = require('gulp-sass');
+
+gulp.task('compile', function() {
+  gulp.src('src/templates/index.html')
+    .pipe(data(function() {
+      var mdfile = fs.readFileSync('./src/content/index.md', 'utf8');
+      console.log(mdfile);
+      return { post: mdfile };
+    }))
+    .pipe(template())
+    .pipe(concat('index.html'))
+    .pipe(gulp.dest('./'));
+});
+ 
+// gulp.task('compile', function() {
+//   connect.server({
+//     livereload: true
+//   });
+// });
+ 
+// gulp.task('less', function() {
+//   gulp.src('styles/main.less')
+//     .pipe(less())
+//     .pipe(gulp.dest('styles'))
+//     .pipe(connect.reload());
+// });
+ 
+// gulp.task('watch', function() {
+//   gulp.watch('styles/*.less', ['less']);
+// })
+ 
+// gulp.task('default', ['less', 'webserver', 'watch']);
+
+gulp.task('default', ['compile']);
